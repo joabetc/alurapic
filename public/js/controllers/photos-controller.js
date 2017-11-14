fbd3259ed813b2ddd7481af06b672634f4dbb7a8
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-11-14 18:19:20 -0200

    Building the foundation of the application

---
+++ public/js/controllers/photos-controller.js
@@ -0,0 +1,8 @@
+angular.module('alurapic').controller('PhotosController', function($scope) {
+
+  $scope.photo = {
+    title: 'Lion',
+    url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+  };
+
+});
