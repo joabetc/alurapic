c52e48eaf7f2079ce491695ceb24d92ab9c7a004
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-11-16 18:30:59 -0200

    Using ng-repeat and $http

--- public/js/controllers/photos-controller.js
+++ public/js/controllers/photos-controller.js
@@ -1,8 +1,18 @@
-angular.module('alurapic').controller('PhotosController', function($scope) {
+angular.module('alurapic').controller('PhotosController', function($scope, $http) {
 
-  $scope.photo = {
-    title: 'Lion',
-    url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-  };
+  $scope.photos = [];
+
+  $http.get('v1/fotos').success(function(photos) {
+    $scope.photos = photos;
+  }).error(function(err) {
+    console.log(err);
+  });
+
+  /*var promise = $http.get('v1/fotos');
+  promise.then(function(ret) {
+    $scope.photos = ret.data;
+  }).catch(function(err) {
+    console.log(err);
+  });*/
 
 });
