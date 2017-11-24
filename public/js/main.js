1c25abe90b2b0e227f6a1a8b57325f03025f9c8d
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-11-24 18:34:15 -0200

    Creating routes and partials

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,18 @@
-angular.module('alurapic', ['myDirectives', 'ngAnimate']);
+angular.module('alurapic', ['myDirectives', 'ngAnimate', 'ngRoute'])
+  .config(function($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+    
+    $routeProvider
+      .when('/photos', {
+        templateUrl: 'partials/principal.html',
+        controller: 'PhotosController'
+      })
+      .when('/photos/new', {
+        templateUrl: 'partials/photo.html'
+      })
+      .otherwise({
+        redirectTo: '/photos'
+      });
+
+  });
