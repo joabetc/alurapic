27c684437901dab44b56d3e2b3039a0551aa8484
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-11-24 18:41:08 -0200

    Creating a new directive for image tag

--- public/js/directives/my-directives.js
+++ public/js/directives/my-directives.js
@@ -15,4 +15,18 @@ angular
       ddo.templateUrl = 'js/directives/my-panel.html';
 
       return ddo;
+    })
+    .directive('myPhoto', function() {
+      var ddo = {};
+
+      ddo.restric = "AE";
+
+      ddo.scope = {
+        title: '@',
+        url: '@'
+      };
+
+      ddo.template = '<img class="img-responsive center-block" ng-src="{{url}}" alt="{{titulo}}">';
+
+      return ddo;
     });
