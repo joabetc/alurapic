7f0c209b99811446ae7eb5fd24b7558703d91c40
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-11-28 18:23:51 -0200

    Validating form

--- public/js/controllers/photo-controller.js
+++ public/js/controllers/photo-controller.js
@@ -1,8 +1,20 @@
-angular.module('alurapic').controller('PhotoController', function($scope) {
+angular.module('alurapic').controller('PhotoController', function($scope, $http) {
   
   $scope.photo = {};
+  $scope.message = '';
 
   $scope.submit = function() {
-    console.log($scope.photo);
+    if ($scope.formulario.$valid) {
+
+      $http.post('v1/fotos', $scope.photo)
+        .success(function() {
+          $scope.photo = {};
+          $scope.message = 'Foto incluída com sucesso';
+        })
+        .error(function(error) {
+          $scope.message = 'Não foi possível incluir a foto';
+          console.log(error);
+        });
+    }
   }
 });
